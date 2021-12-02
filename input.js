f883ae4d2c1f12db30ee176654d99bf079ddad4d
moh-diab <mohdiab@gmail.com>
2021-12-01 20:43:06 -0500

    WASD Movement

--- input.js
+++ input.js
@@ -1,9 +1,7 @@
-const handleUserInput = function(key) {
-  if(key === '\u0003')
-  process.exit();  
-}
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,5 +9,20 @@ const setupInput = function () {
   stdin.on('data', handleUserInput);
   return stdin;
 };
+
+const handleUserInput = function (key) {
+  if (key === '\u0003')
+    process.exit();
+  else if (key === 'w') {
+    connection.write("Move: up")
+  } else if (key === 'a') {
+    connection.write("Move: left")
+  } else if (key === 's') {
+    connection.write("Move: down")
+  } else if (key === 'd') {
+    connection.write("Move: right")
+  }
+};
+
 
 module.exports = { setupInput }
